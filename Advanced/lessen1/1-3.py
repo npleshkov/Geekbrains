0dee1ac8479bacd5b5712c7994a6656fc4b13063
npleshkov <npleshkov@gmail.com>
2019-09-06 17:01:14 +0400

    add requirements.txt

---
+++ Advanced/lessen1/1-3.py
@@ -0,0 +1,14 @@
+st = ('разработка', 'сокет', 'декоратор')
+
+# b'разработка'
+# b'сокет'
+print(type(b'test'))
+
+# for i in st:
+#
+#     # print(f' Тип {type(b'i')},  длина строки {len(i)}')
+#     if 'Some string' == b''i'':
+#         print('1')
+#     else:
+#         print(2)
+
