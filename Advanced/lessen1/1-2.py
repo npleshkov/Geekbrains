2213116ff4e819152b08ccba6304f5c5e5ce74e8
npleshkov <npleshkov@gmail.com>
2019-09-09 12:09:19 +0400

    add project

--- Advanced/lessen1/1-2.py
+++ Advanced/lessen1/1-2.py
@@ -3,12 +3,3 @@ st = ('разработка', 'сокет', 'декоратор')
 for i in st:
     t = bytes(i, encoding='utf-8')
     print(t.decode())
-
-# for i in st:
-#
-#     # print(f' Тип {type(b'i')},  длина строки {len(i)}')
-#     if 'Some string' == b''i'':
-#         print('1')
-#     else:
-#         print(2)
-#
--- Advanced/lessen1/1-3.py
+++ Advanced/lessen1/1-3.py
@@ -1,8 +1,8 @@
 st = ('разработка', 'сокет', 'декоратор', 'function', 'method')
 
 for i in st:
-    u = bytes(i, encoding='utf-8')
+    u = i.encode('utf-8')
     # l = bytes(i, encoding='latin-1')
-    print(u.decode())
+    print(u.decode('utf-8'))
     # print(l.decode())
 
