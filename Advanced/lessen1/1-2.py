9ea4532d5a999e405600985c776ad0dd9ca169ba
npleshkov@gmail.com <Silver_2009>
2019-09-09 20:21:24 +0400

    v1

--- Advanced/lessen1/1-2.py
+++ Advanced/lessen1/1-2.py
@@ -4,11 +4,3 @@ for i in st:
     t = bytes(i, encoding='utf-8')
     print(t.decode())
 
-# for i in st:
-#
-#     # print(f' Тип {type(b'i')},  длина строки {len(i)}')
-#     if 'Some string' == b''i'':
-#         print('1')
-#     else:
-#         print(2)
-#
